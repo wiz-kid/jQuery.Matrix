f72ff6231ac1cb43818bd9027e5918912bd04af5
wiz-kid <aditya.adiioo7@gmail.com>
2013-06-16 15:44:46 +0530

    Update jQuery.Matrix.js

--- jQuery.Matrix.js
+++ jQuery.Matrix.js
@@ -3,6 +3,9 @@ $.fn.Matrix = function (options) {
     container.addClass("matrix");
     container.children().each(function () {
         $(this).css("-webkit-animation-duration", Math.floor((Math.random() * 20) + 3) + "s").
+    	css("animation-duration", Math.floor((Math.random() * 20) + 3) + "s").
+        css("-ms-animation-duration", Math.floor((Math.random() * 20) + 3) + "s").
+		css("-moz-animation-duration", Math.floor((Math.random() * 20) + 3) + "s").
         css("left", Math.floor((Math.random() * parseInt($(container).width())) + 10)).
         css("font-size", Math.floor((Math.random() * 20) + 10) + "px");
     });
