2c5fd9643ecbba0ff89277b634a7cbda9c99ce46
wiz-kid <aditya.adiioo7@gmail.com>
2013-06-16 14:59:21 +0530

    Create jQuery.Matrix.js

---
+++ jQuery.Matrix.js
@@ -0,0 +1,9 @@
+$.fn.Matrix = function (options) {
+    var container = $(this);
+    container.addClass("matrix");
+    container.children().each(function () {
+        $(this).css("-webkit-animation-duration", Math.floor((Math.random() * 20) + 3) + "s").
+        css("left", Math.floor((Math.random() * parseInt($(container).width())) + 10)).
+        css("font-size", Math.floor((Math.random() * 20) + 10) + "px");
+    });
+};
